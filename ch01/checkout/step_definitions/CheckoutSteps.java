86f281d4e0a540d702cfd6c4ed2d25d16431f140
Juan Hernandez <jiniguez@foundationmedicine.com>
2017-06-29 17:24:23 -0500

    junit assert added, 3rd step implemented, domain clas defining a total method

--- ch01/checkout/step_definitions/CheckoutSteps.java
+++ ch01/checkout/step_definitions/CheckoutSteps.java
@@ -1,26 +1,32 @@
 package step_definitions;
 
+import implementation.Checkout;
+
 import cucumber.api.java.en.*;
 import cucumber.api.PendingException;
 
+import static org.junit.Assert.*;
+
 public class CheckoutSteps{
 
-@Given("^the price of a \"([^\"]*)\" is (\\d+)c$")
-public void thePriceOfAIsC(String name, int price) throws Throwable {
-    int bananaPrice = price;
-}
+	int bananaPrice = 0;
+	Checkout checkout;
 
-@When("^I checkout (\\d+) \"([^\"]*)\"$")
-public void iCheckout(int arg1, String arg2) throws Throwable {
-    Checkout checkout = new Checkout();
-    checkout.add(itemCount, bananaPrice);
-}
+    @Given("^the price of a \"([^\"]*)\" is (\\d+)c$")
+    public void thePriceOfAIsC(String name, int price) throws Throwable {
+        bananaPrice = price;
+    }
 
-@Then("^the total price should be equal to (\\d+)c$")
-public void theTotalPriceShouldBeEqualToC(int arg1) throws Throwable {
-    // Write code here that turns the phrase above into concrete actions
-    throw new PendingException();
-}
+    @When("^I checkout (\\d+) \"([^\"]*)\"$")
+    public void iCheckout(int itemCount, String itemName) throws Throwable {
+        checkout = new Checkout();
+        checkout.add(itemCount, bananaPrice);
+    }
+
+    @Then("^the total price should be equal to (\\d+)c$")
+    public void theTotalPriceShouldBeEqualToC(int total) throws Throwable {
+        assertEquals(total, checkout.total());
+    }
 
 
 }
